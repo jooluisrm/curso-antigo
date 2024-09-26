e904d73710ec9a3a1c6098ad6a7cad987342dba9
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2024-09-26 13:50:47 -0300

    Loidando com loading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 "use client"
 
 import { Posts } from "@/Types/Posts";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export const Page = () => {
 
     const [posts, setPosts] = useState<Posts[]>([]);
-
+    const [loading, setLoading] = useState(false);
+    /*
     const handleLoadButton = () => {
         fetch('https://jsonplaceholder.typicode.com/posts')
             .then((response) => {
@@ -16,18 +17,40 @@ export const Page = () => {
                 setPosts(json);
             })
     }
+    */
+
+    useEffect(() => {
+        handleLoadButton();
+    }, []);
+
+    const handleLoadButton = async () => {
+        setLoading(true);
+        let response = await fetch('https://jsonplaceholder.typicode.com/posts');
+        let json = await response.json();
+        setLoading(false);
+        setPosts(json);
+    }
     return (
-        <div className="bg-white text-black h-full w-full">
+        <div className="bg-white text-black min-h-screen w-full">
             <button className="block bg-blue-400 p-2 rounded-md" onClick={handleLoadButton}>Carregar Posts</button>
-            Total de Posts: {posts.length}
-            <div>
-                {posts.map((post, index) => (
-                    <div className="bg-slate-500 my-3" key={index}>
-                        <h1 className="text-4xl">{post.title}</h1>
-                        <div>{post.body}</div>
+
+            {loading &&
+                <div>Carregando!!!</div>
+            }
+
+            {!loading &&
+                <>
+                    <div>Total de Posts: {posts.length}</div>
+                    <div>
+                        {posts.map((post, index) => (
+                            <div className="bg-slate-500 my-3" key={index}>
+                                <h1 className="text-4xl">{post.title}</h1>
+                                <div>{post.body}</div>
+                            </div>
+                        ))}
                     </div>
-                ))}
-            </div>
+                </>
+            }
         </div>
 
     );

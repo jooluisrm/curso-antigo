211bc641682c60418154c780f50ca757c561d0f4
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2024-09-27 15:11:39 -0300

    Usando o Axios

--- src/api.ts
+++ src/api.ts
@@ -1,12 +1,30 @@
-const BASE = 'https://jsonplaceholder.typicode.com';
+import axios from "axios";
+
+const http = axios.create({
+    baseURL: 'https://jsonplaceholder.typicode.com',
+
+})
 
 export const Api = {
     getAllPosts: async () => {
+        let response = await http.get('/posts');
+        return response.data;
+
+        /* 
         let response = await fetch(`${BASE}/posts`);
         let json = await response.json();
         return json;
+        */
     },
     addNewPost: async (title: string, body: string, userId: number) => {
+        let response = await http.post(`/posts`, {
+            title,
+            body,
+            userId
+        });
+        return response.data;
+
+        /*
         let response = await fetch(`${BASE}/posts`, {
             method: "POST",
             body: JSON.stringify({
@@ -20,5 +38,6 @@ export const Api = {
         });
         let json = await response.json();
         return json;
+        */
     }
 }
